f84b692e8bf784fbf3b782b27251b01456feec21
Arshad Khan <arshdkhn1@gmail.com>
2018-08-19 20:51:14 +0530

    autonomous and custom elements

---
+++ words-count/main.js
@@ -0,0 +1,29 @@
+class WordCount extends HTMLParagraphElement {
+    constructor() {
+        super()
+
+        const wcParent = this.parentNode
+
+        const countWords = node => {
+            const text = node.innerText || node.textContent
+            return text.split(/\s+/g).length
+        }
+
+        console.log(wcParent)
+
+        const getCount = () => `Words: ${countWords(wcParent)}`
+
+        const shadow = this.attachShadow({mode: 'open'})
+
+        const text = document.createElement('span')
+        text.textContent = getCount()
+
+        shadow.appendChild(text)
+
+        setInterval(() => {
+            text.textContent = getCount()
+        })
+    }
+}
+
+customElements.define('word-count', WordCount, {extends: 'p'})
